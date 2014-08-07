8f1b76c5eeb90c93fde547a199b92a3c54be2165
wangxian <xian366@126.com>
2014-08-07 17:27:49 +0800

    CHANGED: finished model bind dom

--- examples/index.md
+++ examples/index.md
@@ -8,15 +8,21 @@
 
 <div id="main">
   <form action="">
-    <h3><label for="name">请输入姓名：</label><h3>
+    <h3><label for="nickname">请输入姓名：</label><h3>
     <p>
-      <input id="name" type="text" name="name" 
-             vm="val:name, on:change=updateMe, on:click=showTips" />
+      <input id="nickname" type="text" name="nickname" 
+            vm="val:nickname, 
+                 on:change=updateMe, 
+                 on:click=showTips" />
     </p>
     
     <h3>您的姓名为：</h3>
-    <p vm="html:name">--</p>
-    <p vm="text:name">--</p>
+    
+    1. 使用html绑定
+    <p vm="html:nickname">--</p>
+    
+    2. 使用text绑定
+    <p vm="text:nickname">--</p>
     
   </form>
 </div>
@@ -24,40 +30,47 @@
 <script>
 define("main", function(require, exports, module){
   var $        = require("jquery");
-  var Backbone = require("backbone.vm");
+  var Backbone = require("src/backbone.vm");
   
   // for test
   window.$ = $;
   window.Backbone = Backbone;
   
-  console.dir(new (Backbone.VM.extend({"id":12})) )
+  // console.dir(new (Backbone.VM.extend({"id":12})) )
   
   // console.log(jQuery);
   
+  var MainApp = Backbone.VM.extend({
+    
+    // default vm value
+    defaults: {
+      "nickname": "<b>antony</b>",
+      "friends" : ["sa", "sb", "sc"]
+    },
+
+    // Bind Dom id, Control of the scope of the VM
+    // So, you can define some VM object
+    el: "#main",
+    
+    // At initialization we do something
+    initialize: function() {
+      // do something...
+    },
+    
+    // when vm.name changed
+    updateMe: function() {
+    
+    },
+    
+    // when input[name] click, do this function
+    showTips: function(e) {
+      // modify name's value
+      this.vm.set("name", "antony");
+    }
+  });
   
-
-  // var MainApp = Backbone.VM.extend({
-
-  //   // Bind Dom id, Control of the scope of the VM
-  //   // So, you can define some VM object
-  //   el: "#main",
-    
-  //   // At initialization we do something
-  //   initialize: function() {
-  //     // do something...
-  //   },
-    
-  //   // when vm.name changed
-  //   updateMe: function() {
-    
-  //   },
-    
-  //   // when input[name] click, do this function
-  //   showTips: function(e) {
-  //     // modify name's value
-  //     this.vm.set("name", "xxxxx");
-  //   }
-  // });
+  // init new MainApp
+  window.mainapp = new MainApp();
   
   
 });
--- src/backbone.vm.js
+++ src/backbone.vm.js
@@ -5,24 +5,95 @@
 //  For all details and documentation:
 //  http://github.com/wangxian/backbone.vm
 
-
 var $ = require("jquery");
 var _ = require("underscore");
 var Backbone = require("backbone");
 
+// List of view options to be merged as properties.
+var vmOptions = ['el', 'defaults'];
+
+// Cached regex for stripping vm node attributes. space, \r \n
+var vmAttrStripper = /\s+/g;
+
 // Create a plugin from backbone.js, Similar usage and backbone.view
 var VM = Backbone.VM = function(options) {
-  // console.info(options);
+  this.cid = _.uniqueId('vm');
+  if(!options) { options = {}; }
+  _.extend(this, _.pick(options, vmOptions));
+
+  this.$el = $(this.el);
   this.scanAttrs();
+
+  this.vm.on("change", this.updateVM, this);
+  this.vm.set(this.defaults);
+
+  this.initialize.apply(this, arguments);
 };
 
 _.extend(VM.prototype, {
 
-  // Store the relationship between Dom and model
-  attrs: null,
+  // Set default vm scope,
+  // When user's app not defined, set the defulat el 'body'
+  el: "body",
 
+  // Wrapper el using jQuery
+  $el: null,
+
+  // Store the relationship between Dom and model
+  // eg, { "name":[{"$el": xx, ...}], ... }
+  attrs: {},
+
+  // Default VM value
+  defaults: {},
+
+  // Update VM bind node when vm model is updated
+  updateVM: function(model) {
+    // console.info("model updated:", model.changed, model.toJSON());
+    var it = this;
+    var attrs = _.pick(this.attrs, _.keys(model.changed));
+    _.each(attrs, function(v, k){
+      _.each(v, function(func){
+        func(it.vm.get(k));
+      });
+    });
+  },
+
+  // VM's model, Your app can use it to set VM value
+  // eg, this.vm.set("name", "tom")
+  vm: new Backbone.Model(),
+
+  // Scan html dom attribute contains vm="*"
   scanAttrs: function() {
-    console.info("scan all attributes");
+    var it = this;
+
+    var $vmAttrs = this.$el.find("[vm]");
+    $vmAttrs.each(function(k, node){
+      // VM HTML node
+      var vmNode = node.getAttribute("vm").replace(vmAttrStripper, "").split(",");
+      _.each(vmNode, function(v){
+        var arr = v.split(":");
+        if(arr[0] === "on") {
+
+        } else if(arr[0] === "show") {
+
+        } else if(arr[0] === "for") {
+
+        } else {
+          // html, text, val...
+          if(! it.attrs[ arr[1] ] ) it.attrs[ arr[1] ] = [];
+
+          //@TODO: 可优化，编译模板
+          it.attrs[ arr[1] ].push(
+            (function(node, funcName){
+              return function(value) {
+                node[funcName](value);
+              };
+            })( $(node), arr[0] )
+          );
+        }
+
+      });
+    });
   },
 
 
@@ -35,9 +106,8 @@ _.extend(VM.prototype, {
 // Set up inheritance for the VM
 // Similar to `goog.inherits`, but uses a hash of prototype properties and
 // class properties to be extended.
-VM.extend = Backbone.View.extend;
+VM.extend = Backbone.Model.extend;
 
 // Return a CMD module from seajs,
 // It must be loaded after backbone.js
 module.exports = Backbone;
-
